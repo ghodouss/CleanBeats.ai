1dc275cac1fa4b92537f47c7a3be2295007e2eb6
Kian Ghodoussi <ghodouss@usc.edu>
2018-01-19 23:55:15 -0800

    added unique naming to new audio file

--- censor.py
+++ censor.py
@@ -1,16 +1,19 @@
 from pydub import AudioSegment
 from pydub.playback import play
+import time
 
 # read in audio file and get the two mono tracks
 
 def censor(file_name,snips=[{"start":20,"end":25},{"start":30,"end":35}]):
 
+	name = str(time.time()) + ".mp3"
+
 	sound_stereo = AudioSegment.from_file(file_name, format="mp3")
 	words=snips
 	for i in words:
-		start = max(i["start"]-.3, 0)*1000
-		end = min((i["end"]+.2)*1000, len(sound_stereo)-1)
-		samp=sound_stereo[start, end]
+		start = int(max(i["start"]-.1, 0)*1000)
+		end = int(min((i["end"]+.1)*1000, len(sound_stereo)-1))
+		samp=sound_stereo[start: end]
 		sound_monoL = samp.split_to_mono()[0]
 		sound_monoR = samp.split_to_mono()[1]
 		# Invert phase of the Right audio file
@@ -22,7 +25,8 @@ def censor(file_name,snips=[{"start":20,"end":25},{"start":30,"end":35}]):
 
 
 	# Export merged audio file
-	fh = sound_stereo.export("output.mp3", format="wav")
+	fh = sound_stereo.export(name, format="wav")
 
-	return sound_stereo
+	return name
+
 
