84639ef090654f76b550f79bbdcfd77a94ebcfcd
Kian Ghodoussi <ghodouss@usc.edu>
2018-01-19 23:27:08 -0800

    expanded breadth of blur

--- censor.py
+++ censor.py
@@ -8,9 +8,9 @@ def censor(file_name,snips=[{"start":20,"end":25},{"start":30,"end":35}]):
 	sound_stereo = AudioSegment.from_file(file_name, format="mp3")
 	words=snips
 	for i in words:
-		s=i["start"]
-		e=i["end"]
-		samp=sound_stereo[float(s)*1000:float(e)*1000]
+		start = max(i["start"]-.3, 0)*1000
+		end = min((i["end"]+.2)*1000, len(sound_stereo)-1)
+		samp=sound_stereo[start, end]
 		sound_monoL = samp.split_to_mono()[0]
 		sound_monoR = samp.split_to_mono()[1]
 		# Invert phase of the Right audio file
@@ -18,9 +18,11 @@ def censor(file_name,snips=[{"start":20,"end":25},{"start":30,"end":35}]):
 	
 		# Merge two L and R_inv files, this cancels out the centers
 		sound_CentersOut = sound_monoL.overlay(sound_monoR_inv)
-		sound_stereo=sound_stereo[:float(s)*1000]+sound_CentersOut+sound_stereo[float(e)*1000:]
+		sound_stereo = sound_stereo[:start]+sound_CentersOut+sound_stereo[end:]
 
 
 	# Export merged audio file
-	fh = sound_stereo.export("output.mp3", format="mp3")
+	fh = sound_stereo.export("output.mp3", format="wav")
+
+	return sound_stereo
 
