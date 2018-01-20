28473e82f660ea76526b70e14e9472aae09d8026
Tejas Ramdas <tejas.ramdas@gmail.com>
2018-01-19 22:59:53 -0800

    pyduh boiiz

---
+++ pyduh.py
@@ -0,0 +1,23 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+# read in audio file and get the two mono tracks
+sound_stereo = AudioSegment.from_file("sample.mp3", format="mp3")
+words=[{"start":20,"end":25},{"start":30,"end":35}]
+for i in words:
+	s=i["start"]
+	e=i["end"]
+	samp=sound_stereo[float(s)*1000:float(e)*1000]
+	sound_monoL = samp.split_to_mono()[0]
+	sound_monoR = samp.split_to_mono()[1]
+	# Invert phase of the Right audio file
+	sound_monoR_inv = sound_monoR.invert_phase()
+	
+	# Merge two L and R_inv files, this cancels out the centers
+	sound_CentersOut = sound_monoL.overlay(sound_monoR_inv)
+	sound_stereo=sound_stereo[:float(s)*1000]+sound_CentersOut+sound_stereo[float(e)*1000:]
+
+
+# Export merged audio file
+fh = sound_stereo.export("output.mp3", format="mp3")
+
