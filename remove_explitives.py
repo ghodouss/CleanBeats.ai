4be8aa0e55199ba77236a0b513f7945876e013a5
Kian Ghodoussi <ghodouss@usc.edu>
2018-01-19 22:53:53 -0800

    updated to use align.py

--- remove_explitives.py
+++ remove_explitives.py
@@ -1,22 +1,41 @@
-from run_gentle import run_gentle
+from canetis.align import align
+
+
+def segs_to_gentle(segments):
+    gentle_output = []
+
+    for seg in segments:
+        gentle_output += seg.gentle_output
+
+    return gentle_output
 
 
 
 
-def get_all_explitive_times(audio_file_path, transcript):
+def get_all_explitive_times(audio_file_path, text_file_path):
 
     explitives = set()
     explitives.add("fuck")
     explitives.add("shit")
     explitive_times = []
+
+
     
-    gentle_output = run_gentle(audio_file_path, transcript)
+    segments = align(audio_file_path, text_file_path)
+    gentle_output = segs_to_gentle(segments)
+
+    print(gentle_output)
 
     for word in gentle_output:
         if word.word in explitives:
-            explitive_time = {"start":word.start, "end":word.end}
+            explitive_time = {"word":word.word, "start":word.start, "end":word.end}
             explitive_times.append(explitive_time)
     
     return explitive_times
 
-get_all_explitive_times("eminem_curse.mp3", "em_lyrics.txt")
+
+print(get_all_explitive_times("eminem_curse.mp3", "em_lyrics.txt"))
+
+
+
+
